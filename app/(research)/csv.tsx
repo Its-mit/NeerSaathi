c022d06351d90dd7cd467151ae4e0bd44aa8833d
Arpan110 <arpan0325@gmail.com>
2025-09-07 17:39:14 +0530

    updated data

---
+++ app/(research)/csv.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ResearchCSV() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Researcher: Monthly CSV Download</Text>
+    </View>
+  );
+}
